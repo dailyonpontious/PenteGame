311e3844547a7d4ea9106320d42b4ee5e72fbe2b
Dailyon <dpontious@student.neumont.edu>
2025-01-21 09:40:30 -0700

    add a controller and a player class

---
+++ src/main/java/csc180/pontious/dailyon/pentegame/Controller/PenteController.java
@@ -0,0 +1,10 @@
+/**
+ * @author dpontious
+ * @createdOn 1/21/2025 at 9:39 AM
+ * @projectName PenteGame
+ * @packageName csc180.pontious.dailyon.pentegame.Controller;
+ */
+package csc180.pontious.dailyon.pentegame.Controller;
+
+public class PenteController {
+}
---
+++ src/main/java/csc180/pontious/dailyon/pentegame/Model/Player.java
@@ -0,0 +1,10 @@
+/**
+ * @author dpontious
+ * @createdOn 1/21/2025 at 9:40 AM
+ * @projectName PenteGame
+ * @packageName csc180.pontious.dailyon.pentegame.Model;
+ */
+package csc180.pontious.dailyon.pentegame.Model;
+
+public class Player {
+}
